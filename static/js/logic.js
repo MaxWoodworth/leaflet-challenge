a28aeade005e977b07369adf47b02a7d9e1b5d24
Max Woodworth <max10woodworth@gmail.com>
2021-02-18 14:04:14 -0600

    creates color spectrum

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,5 +15,38 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 //Give reference to chosen json URL (M1.0+ Earthquakes)
-//var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson"
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson"
 
+//create color options
+//link to color scalers https://stackoverflow.com/questions/22893789/d3-color-scale-linear-with-multiple-colors
+//colorscale = d3.scaleLinear()
+    //.domain([-1, 0, 1])
+    //.range(["red", "white", "green"])
+
+//set up d3
+d3.json(url, function(output) {
+    //create forloop
+    for (var axis = 0; axis < output.features.length; axis++) {
+        //set colors for circle plots
+        var color = "";
+        if (base > 90) {
+            color = "red"
+        }
+        else if (depth > 75) {
+            color = "orange";
+        }
+        else if (depth > 50) {
+            color = "yellow";
+        }
+        else if (depth > 25) {
+            color = "blue";
+        }
+        else if (depth > 10) {
+            color = "purple";
+        }
+        else {
+            color = "white";
+        }
+    
+    }
+})
